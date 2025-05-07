6e0989c3ec69fe1677761a293511405452050869
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-07 13:41:08 -0300

    feat: Added Product Model

---
+++ backend/BarCodeAPI/BarCodeAPI/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace BarCodeAPI.Models;
+
+public class Product {
+    public int ProductId { get; set; }
+    public string? Name { get; set; } 
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? BarCode { get; set; }
+}
