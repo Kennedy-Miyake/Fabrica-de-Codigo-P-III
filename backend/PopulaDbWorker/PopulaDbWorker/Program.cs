3c35b587fb5f7aa6a6bc5493b1ffe07379b55cd4
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-13 02:47:39 -0300

    feat: created a worker solution to populate database

---
+++ backend/PopulaDbWorker/PopulaDbWorker/Program.cs
@@ -0,0 +1,11 @@
+namespace PopulaDbWorker;
+
+public class Program {
+    public static void Main(string[] args) {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<Worker>();
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
