b85f0d21c8946ea4a7b2d86a7f3ecc4d2c1d3e50
takiguchi <alexandre.pupim360@gmail.com>
2025-05-12 21:20:07 -0300

    feat: create sidebar in navbar
    The “scan” button has been removed and the “Menu” button has been added inside the “navbar” component. When triggered, it calls up the sidebar menu with the page paths.

--- frontend/Barcode/src/components/NavbarComponent.vue
+++ frontend/Barcode/src/components/NavbarComponent.vue
@@ -1,11 +1,28 @@
 <template>
+  <div>
     <nav class="bg-red-600 text-white py-4 px-8 flex justify-between items-center">
-        <h1 class="text-lg font-bold">Barcode Project</h1>
-        <ul class="flex gap-6">
-            <li>
-                <router-link to="/" class="hover:underline">Scan</router-link>
-            </li>
-           
-        </ul>
+      <h1 class="text-lg font-bold">Barcode Project</h1>
+      <button @click="toggleSidebar" class="hover:underline">
+        Menu
+      </button>
     </nav>
+    <SideBarComponent v-if="isSidebarOpen" @close="toggleSidebar" />
+  </div>
 </template>
+
+<script>
+import { isSidebarOpen, toggleSidebar } from '../assets/Services/SideBar.js'
+import SideBarComponent from './SideBarComponent.vue'
+
+export default {
+  components: {
+    SideBarComponent,
+  },
+  setup() {
+    return {
+      isSidebarOpen,
+      toggleSidebar,
+    }
+  },
+}
+</script>
