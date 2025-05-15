a96005e3d2247fbde0cd635fca3bd3d4af7f9238
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-15 04:38:02 -0300

    test: testing migrations
    migrations tested. generation and application working correctly.

--- backend/Shared/BarCode.Infrastructure/BarCode.Infrastructure/Migrations/20250515073439_TestMigration.Designer.cs
+++ backend/Shared/BarCode.Infrastructure/BarCode.Infrastructure/Migrations/20250515073439_TestMigration.Designer.cs
@@ -4,16 +4,19 @@ using BarCode.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BarCodeAPI.Migrations
+namespace BarCode.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250515073439_TestMigration")]
+    partial class TestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
