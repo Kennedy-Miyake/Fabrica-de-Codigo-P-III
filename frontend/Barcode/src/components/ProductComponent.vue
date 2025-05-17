7bc084c7787734c7b624554465a1ead2d69e9043
takiguchi <alexandre.pupim360@gmail.com>
2025-05-16 23:36:19 -0300

    feat: ProductPage components

---
+++ frontend/Barcode/src/components/ProductComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <p v-if="loading">Carregando...</p>
+      <p v-else-if="error">{{ error }}</p>
+      <div v-else-if="product">
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>Código de barras: {{ product.barcode }}</p>
+      </div>
+      <p v-else>Nenhum produto encontrado.</p>
+    </div>
+</template>
+  
