b16e4d90f55a45562d36c4a6fd644c5c6427bb0d
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-18 03:35:30 -0300

    mod: completely removing Worker from the project
    using the Worker would only make using the API more complex. services can be implemented and managed together with the API

--- backend/PopulaDbWorker/PopulaDbWorker/Services/IBlueSoftCosmosClient.cs
+++
@@ -1,6 +0,0 @@
-// ReSharper disable All
-namespace PopulaDbWorker.Services;
-
-public interface IBlueSoftCosmosClient {
-    Task<string?> GetProductJsonAsync(string barcode, CancellationToken cancellationToken = default);
-}
