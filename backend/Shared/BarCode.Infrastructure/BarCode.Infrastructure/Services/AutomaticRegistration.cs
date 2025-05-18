0c75c71029e86efb5872442d0fe2e1ae6e2122e6
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-18 04:12:09 -0300

    feat: instantiating a product with the IntantiateProduct class

--- backend/Shared/BarCode.Infrastructure/BarCode.Infrastructure/Services/AutomaticRegistration.cs
+++ backend/Shared/BarCode.Infrastructure/BarCode.Infrastructure/Services/AutomaticRegistration.cs
@@ -6,6 +6,19 @@ namespace BarCode.Infrastructure.Services;
 
 public class AutomaticRegistration : IAutomaticRegistration {
     public Product InstantiateProduct(string json) {
+        var document = JsonDocument.Parse(json);
+        var root = document.RootElement;
+
+        var product = new Product {
+            Name = root.GetProperty("description").ToString(),
+            Description = root.GetProperty("ncm")
+                              .GetProperty("full_description")
+                              .ToString(),
+            ImageUrl = root.GetProperty("thumbnail").ToString(),
+            BarCode = root.GetProperty("gtin").ToString()
+        };
+
+        return product;
     }
     public Product FillInProductInformation(Product product) {
         throw new NotImplementedException();
