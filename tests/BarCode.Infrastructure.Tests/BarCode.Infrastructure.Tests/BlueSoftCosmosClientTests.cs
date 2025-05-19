bf0839a8eb793701d0eddaf6ba2ec2d2e5e19f07
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-19 05:01:51 -0300

    feat: creates an HTTP Client with a fake handler
    BaseAddress and UserAgent configured

---
+++ tests/BarCode.Infrastructure.Tests/BarCode.Infrastructure.Tests/BlueSoftCosmosClientTests.cs
@@ -0,0 +1,19 @@
+using System.Net;
+using BarCode.Infrastructure.Services;
+using BarCode.Infrastructure.Tests.Helpers;
+using Microsoft.Extensions.Configuration;
+
+namespace BarCode.Infrastructure.Tests;
+
+public class BlueSoftCosmosClientTests {
+    HttpClient CreateHttpClient(HttpResponseMessage fakeResponse) {
+        var handler = new FakeHttpMessageHandler(fakeResponse);
+        var client = new HttpClient(handler) {
+            BaseAddress = new Uri("https://api.cosmos.bluesoft.com.br/")
+        };
+        client.DefaultRequestHeaders
+              .UserAgent
+              .ParseAdd("BarCodeAPIUnitTests (+https://github.com/Kennedy-Miyake/Fabrica-de-Codigo-P-III.git)");
+        return client;
+    }
+}
