904cc4d0e23334f86513d00602bc479aae2d8120
Racx-DEV <raulfabiano.dev@gmail.com>
2025-05-19 22:00:13 -0300

    feat: add Home route definition

--- frontend/Barcode/src/router/index.js
+++ frontend/Barcode/src/router/index.js
@@ -1,19 +1,27 @@
+/* src/router/index.js */
 import { createRouter, createWebHistory } from 'vue-router'
-import BarcodeView from '../views/BarcodeView.vue'
+
+import HomeView     from '../views/HomeView.vue'           
+import BarcodeView  from '../views/BarcodeView.vue'        
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/',            
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/scanner',     
       name: 'barcode',
       component: BarcodeView,
     },
     {
-      path: '/product',
+      path: '/product',    
       name: 'product',
-      component: () => import('../views/ProductView.vue')
-    }
+      component: () => import('../views/ProductView.vue'),
+    },
   ],
 })
 
