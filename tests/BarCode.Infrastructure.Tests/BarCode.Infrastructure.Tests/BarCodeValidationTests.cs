00084f4f96e6c03e5b4a35b3d95bde6dcd4a9018
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-21 03:41:14 -0300

    mod: modifying the digit check action
    simplifying with a lambda function

--- tests/BarCode.Infrastructure.Tests/BarCode.Infrastructure.Tests/BarCodeValidationTests.cs
+++ tests/BarCode.Infrastructure.Tests/BarCode.Infrastructure.Tests/BarCodeValidationTests.cs
@@ -16,22 +16,11 @@ public class BarCodeValidationTests {
         int expectedCheckDigit = 5;
         
         // Act
-        // Posições ímpares devem ser multiplicados por 1 e somados
-        int firstResult = 0;
-        for (int i = 0; i < (validBarCode.Length)-1; i += 2) {
-            firstResult += (int)char.GetNumericValue(validBarCode[i]) * 1;
-        }
-        // Posições pares devem ser multiplicados por 3 e somados
-        int secondResult = 0;
-        for (int i = 1; i < validBarCode.Length; i += 2) {
-            secondResult += (int)char.GetNumericValue(validBarCode[i]) * 3;
-        }
-        // Soma total
-        int totalSum = firstResult + secondResult; 
-        // Próximo múltiplo de 10
-        int nextMultipleOfTen = (totalSum + 9) / 10 * 10;
-        // Digito verificador
-        int checkDigit = nextMultipleOfTen - totalSum;
+        int checkDigit = ((Func<int>)(() => {
+            var totalSum = SumTotalMultiple(validBarCode);
+            var nextMultipleOfTen = (totalSum + 9) / 10 * 10;
+            return nextMultipleOfTen - totalSum;
+        }))();
         
         // Assert
         Assert.Equal(expectedCheckDigit, checkDigit);
