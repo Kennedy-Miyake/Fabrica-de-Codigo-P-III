7e0e85a77f00092f08f37d57296f1616c6111f86
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-21 04:23:22 -0300

    mod: all tests of the function to validate the check digit passed

--- tests/BarCode.Infrastructure.Tests/BarCode.Infrastructure.Tests/Ean13BarCodeValidatorTests.cs
+++ tests/BarCode.Infrastructure.Tests/BarCode.Infrastructure.Tests/Ean13BarCodeValidatorTests.cs
@@ -2,20 +2,12 @@ using BarCode.Infrastructure.Services;
 
 namespace BarCode.Infrastructure.Tests;
 
-public class BarCodeValidationTests {
-    private int SumTotalMultiple(string barCode, int position = 0) {
-        int length = barCode.Length - 1;
-        if (position == length) return 0;
-        if (position % 2 == 0) {
-            return ((int)char.GetNumericValue(barCode[position]) * 1) + SumTotalMultiple(barCode, position + 1);
-        }
-        return ((int)char.GetNumericValue(barCode[position]) * 3) + SumTotalMultiple(barCode, position + 1);
-    }
+public class Ean13BarCodeValidatorTests {
     [Fact]
     public void IsValid_ValidBarCode_ReturnsTrue() {
         // Arrange
         string validBarCode = "7891910010905";
-        var barCodeValidation = new BarCodeValidation();
+        var barCodeValidation = new Ean13BarCodeValidator();
         
         // Act
         bool isValid = barCodeValidation.IsValid(validBarCode);
