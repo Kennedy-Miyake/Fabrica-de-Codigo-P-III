511f0b104b5cb05647b4325e4ecc75eae0ce0416
Racx-DEV <raulfabiano.dev@gmail.com>
2025-05-22 17:48:18 -0300

    feat: scaffold Homeview.vue component

---
+++ frontend/Barcode/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+</script>
+
+<template>
+
+  <section class="min-h-screen flex items-center justify-center bg-[#0a1931] text-white">
+    <h1>teste</h1>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
