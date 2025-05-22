5a5e03b0b5c31fb92e4d02c665b5202589802bd7
Racx-DEV <raulfabiano.dev@gmail.com>
2025-05-22 17:55:04 -0300

    style: apply gradient background and flex layout

--- frontend/Barcode/src/views/HomeView.vue
+++ frontend/Barcode/src/views/HomeView.vue
@@ -4,7 +4,7 @@
 
 <template>
 
-  <section class="min-h-screen flex items-center justify-center bg-[#0a1931] text-white">
+  <section class="min-h-screen flex items-center justify-center bg-gradient-to-b from-[#0a1931] via-[#071427] to-black text-white px-6">
     <h1>teste</h1>
   </section>
 </template>
