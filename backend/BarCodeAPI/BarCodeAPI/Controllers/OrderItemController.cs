aac3841657d4d1f9d554c82251f799f01ab6cf46
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-23 05:09:42 -0300

    mod: added database context to OrderItemController

--- backend/BarCodeAPI/BarCodeAPI/Controllers/OrderItemController.cs
+++ backend/BarCodeAPI/BarCodeAPI/Controllers/OrderItemController.cs
@@ -1,9 +1,15 @@
+using BarCode.Infrastructure.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BarCodeAPI.Controllers;
 
 [Route("api/v1")]
 [ApiController]
 public class OrderItemController : ControllerBase {
+    private readonly AppDbContext _context;
     
+    public OrderItemController(AppDbContext context) {
+        _context = context;
+    }
 }
