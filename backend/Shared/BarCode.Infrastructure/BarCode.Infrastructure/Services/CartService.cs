bd2dfdda53af531a0e111bbbbab3b00db3f9084b
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-23 16:00:30 -0300

    feat: added ICartService contract class

---
+++ backend/Shared/BarCode.Infrastructure/BarCode.Infrastructure/Services/CartService.cs
@@ -0,0 +1,9 @@
+using BarCode.Domain.Services;
+
+namespace BarCode.Infrastructure.Services;
+
+public class CartService : ICartService {
+    public decimal CalculateSubtotal(decimal unitaryPrice, int quantity) {
+        throw new NotImplementedException();
+    }
+}
