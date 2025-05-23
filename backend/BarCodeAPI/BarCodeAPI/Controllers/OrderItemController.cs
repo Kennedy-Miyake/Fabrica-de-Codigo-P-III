e67a61e505f7df520269dccd119586d51d05ad60
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-23 05:13:29 -0300

    feat: added Get method to get all OrderItems

--- backend/BarCodeAPI/BarCodeAPI/Controllers/OrderItemController.cs
+++ backend/BarCodeAPI/BarCodeAPI/Controllers/OrderItemController.cs
@@ -1,5 +1,7 @@
 using BarCode.Infrastructure.Context;
 using Microsoft.AspNetCore.Mvc;
+using BarCode.Domain.DTO;
+using BarCode.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BarCodeAPI.Controllers;
@@ -11,5 +13,21 @@ public class OrderItemController : ControllerBase {
     
     public OrderItemController(AppDbContext context) {
         _context = context;
+    }
+
+    [HttpGet("orderitems")]
+    public async Task<ActionResult<IEnumerable<OrderItemDTO>>> GetAllOrderItems() {
+        var orderItems = await _context.OrderItems.AsNoTracking().Take(10).ToListAsync();
+        if (orderItems is null)
+            return NotFound("Itens dos pedidos não encontrados...");
+
+        var orderItemDTOs = orderItems.Select(item => new OrderItemDTO(
+                                                                                      item.OrderItemId,
+                                                                                      item.OrderId,
+                                                                                      item.ProductCompanyId,
+                                                                                      item.Quantity,
+                                                                                      item.UnitaryPrice,
+                                                                                      item.SubTotal)).ToList();
+        return Ok(orderItemDTOs);
     }
 }
