88edd60fc863d94f83ccdfe66fa4ad5c596a3ce9
takiguchi <alexandre.pupim360@gmail.com>
2025-05-24 01:11:57 -0300

    feat: create IEmailValidation/services

--- backend/Shared/BarCode.Infrastructure/BarCode.Infrastructure/Services/EmailValidation.cs
+++ backend/Shared/BarCode.Infrastructure/BarCode.Infrastructure/Services/EmailValidation.cs
@@ -2,6 +2,21 @@ namespace BarCode.Infrastructure.Services;
 
 public class EmailValidation
 {
+// Ele herda diretamente a logica do contrato 
 
+//definir a logica por traz do metodo definido lá no contrato
+
+    public bool IsValid(string email)
+    {
+        // Implementar a lógica de validação de e-mail
+        // Exemplo: verificar se contém "@" e "."
+        return !string.IsNullOrEmpty(email) && email.Contains("@") && email.Contains(".");
+    }
+    public bool IsValidDomain(string email)
+    {
+        // Implementar a lógica de validação de domínio
+        // Exemplo simples: verificar se o domínio é "example.com"
+        return email.EndsWith("@example.com");
+    }
     
 }
