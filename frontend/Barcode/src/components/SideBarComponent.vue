3d880c6baa93a274d64cb993e1efeb5e4dcb2426
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-26 19:41:13 -0300

    feat: created the RegisterProductView view
    view for product registration

--- frontend/Barcode/src/components/SideBarComponent.vue
+++ frontend/Barcode/src/components/SideBarComponent.vue
@@ -27,7 +27,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-blue-900">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
               </svg>
-              <a href="#" class="block p-2 text-gray-800 hover:bg-[#ede8de] rounded">Cadastrar Produto</a>
+              <a href="/admin/register-product" class="block p-2 text-gray-800 hover:bg-[#ede8de] rounded">Cadastrar Produto</a>
             </li>
             <li class="flex items-center gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-blue-900">
