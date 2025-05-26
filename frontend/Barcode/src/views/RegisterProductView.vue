1d2a19122d939c27c3ca8f4e8487c54ce168d7ac
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-26 19:44:45 -0300

    feat: added the ProductForm component. added a method to check if the form has been submitted

---
+++ frontend/Barcode/src/views/RegisterProductView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="flex flex-col text-2xl font-semibold mb-4 items-center justify-center">Cadastrar Produto</h1>
+</template>
+
+<script>
+import ProductForm from '../components/ProductForm.vue'
+
+export default {
+  name: 'RegisterProductView',
+  components: {
+    ProductForm
+  },
+  methods: {
+    onSubmit(formData) {
+      console.log('Form submitted', formData);
+    }
+  }
+}
+</script>
