dad7f87e9d458a1e54cd4d3fa4be38db8b73ce6a
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-26 19:37:47 -0300

    mod: added the submit event to the form

--- frontend/Barcode/src/components/ProductForm.vue
+++ frontend/Barcode/src/components/ProductForm.vue
@@ -1,6 +1,6 @@
 <!-- src/components/ProductForm.vue -->
 <template>
-  <form class="">
+  <form @submit.prevent="handleSubmit" class="max-x-md mx-auto space-y-4 border rounded-lg">
     <!-- Campos do formulário -->
     <!-- Imagem do Produto -->
     <div>
