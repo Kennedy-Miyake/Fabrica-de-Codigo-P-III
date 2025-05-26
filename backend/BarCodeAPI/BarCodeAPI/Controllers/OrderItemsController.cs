0a5fb03c683a53f3f3a0943887f04ad2bf5faf9c
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-26 07:16:49 -0300

    mod: added exception loggers to all OrderItemsController HTTP methods

--- backend/BarCodeAPI/BarCodeAPI/Controllers/OrderItemsController.cs
+++ backend/BarCodeAPI/BarCodeAPI/Controllers/OrderItemsController.cs
@@ -23,8 +23,10 @@ public class OrderItemsController : ControllerBase {
     [HttpGet("orderitems")]
     public async Task<ActionResult<IEnumerable<OrderItemDTO>>> GetAllOrderItems() {
         var orderItems = await _context.OrderItems.AsNoTracking().Take(10).ToListAsync();
-        if (orderItems is null)
-            return NotFound("Itens dos pedidos não encontrados...");
+        if (orderItems is null) {
+            _logger.LogWarning($"Itens dos pedidos não encontrados.");
+            return NotFound($"Itens dos pedidos não encontrados.");
+        }
 
         var orderItemDTOs = orderItems.Select(item => new OrderItemDTO(
                                                                                       item.OrderItemId,
@@ -39,9 +41,11 @@ public class OrderItemsController : ControllerBase {
     [HttpGet("orderitem/{orderItemId:int}", Name = "GetOrderItem")]
     public async Task<ActionResult<OrderItemDTO>> GetOrderItemById(int orderItemId) {
         var orderItem = await _context.OrderItems.AsNoTracking().FirstOrDefaultAsync(p => p.OrderItemId == orderItemId);
-        
-        if (orderItem is null)
-            return NotFound("Item do pedido não encontrado...");
+
+        if (orderItem is null) {
+            _logger.LogWarning($"Item do pedido com ID {orderItemId} não encontrado.");
+            return NotFound($"Item do pedido com ID {orderItemId} não encontrado.");
+        }
 
         var orderItemDTO = new OrderItemDTO(
                                             orderItem.OrderItemId,
@@ -60,8 +64,10 @@ public class OrderItemsController : ControllerBase {
                                        .Where(oi => oi.OrderId == orderId)
                                        .ToListAsync();
 
-        if (!orderItems.Any())
-            return NotFound("Itens dos pedidos não encontrados...");
+        if (!orderItems.Any()) {
+            _logger.LogWarning($"Itens do pedido com ID {orderId} não encontrados.");
+            return NotFound($"Itens do pedido com ID {orderId} não encontrados.");
+        }
         
         var orderItemDTOs = orderItems.Select(item => new OrderItemDTO(
                                                                                       item.OrderItemId,
@@ -75,15 +81,21 @@ public class OrderItemsController : ControllerBase {
 
     [HttpPost("orderitem")]
     public async Task<ActionResult> PostOrderItem([FromBody] OrderItemCreateDTO? dto) {
-        if(dto is null)
-            return BadRequest();
+        if (dto is null) {
+            _logger.LogWarning($"Dados inválidos.");   
+            return BadRequest($"Dados inválidos");
+        }
         var order = await _context.Orders.FindAsync(dto.OrderId);
-        if (order is null)
-            return NotFound("Pedido não encontrado");
+        if (order is null) {
+            _logger.LogWarning($"Pedido com ID {dto.OrderId} não encontrado.");
+            return NotFound($"Pedido com ID {dto.OrderId} não encontrado.");
+        }
         
         var productCompany = await _context.ProductCompanies.FindAsync(dto.ProductCompanyId);
-        if (productCompany is null)
-            return NotFound("Produto não encontrado");
+        if (productCompany is null) {
+            _logger.LogWarning($"Produto com ID {dto.ProductCompanyId} não encontrado.");
+            return NotFound($"Produto com ID {dto.ProductCompanyId} não encontrado.");
+        }
         
         var orderItem = new OrderItem {
             OrderId = dto.OrderId,
