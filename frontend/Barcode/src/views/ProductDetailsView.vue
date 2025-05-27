4dbaa8a48f63699ad57ea4d4cad0cf2afcb05328
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-27 03:24:11 -0300

    feat: added to the setup script product search via the barcode inserted in the search bar

--- frontend/Barcode/src/views/ProductDetailsView.vue
+++ frontend/Barcode/src/views/ProductDetailsView.vue
@@ -1,2 +1,27 @@
 <template>
-</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { getProductByBarcode } from '../assets/services/products.js'
+
+const route = useRoute()
+const barcode = route.params.barcode
+
+const product = ref(null)
+const loading = ref(true)
+const error = ref('')
+
+onMounted(async () => {
+  try {
+    const { data } = await getProductByBarcode(barcode)
+    product.value = data
+    console.log(product.value)
+  } catch(error) {
+    console.error(error)
+    error.value = 'Não foi possível carregar o produto.'
+  } finally {
+    loading.value = false
+  }
+});
+</script>
