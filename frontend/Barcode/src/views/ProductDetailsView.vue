c7468a116637b885325932301603da5b325910bf
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-28 06:55:59 -0300

    mod: added the card of the companies to be printed on canvas. referencing the company props

--- frontend/Barcode/src/views/ProductDetailsView.vue
+++ frontend/Barcode/src/views/ProductDetailsView.vue
@@ -17,6 +17,10 @@
   <section class="bg-red-500 mt-6 max-w-1xl w-full mx-auto">
     <h1 class="text-2xl font-semibold text-black max-w-md mx-auto">Empresas que vendem o produto</h1>
     <div class="flex bg-blue-600 h-[400px] justify-center items-center">
+      <CompanyCardComponent
+        v-for="c in productCompanies"
+        :company="c"
+      />
     </div>
   </section>
 </template>
