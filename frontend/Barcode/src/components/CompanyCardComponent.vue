8aebe92d76788b5afd502c717fb93df941287b5a
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-28 03:29:02 -0300

    feat: created a component for companies separately

---
+++ frontend/Barcode/src/components/CompanyCardComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-gray-500 w-[300px] h-[350px] m-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 m-4 ml-auto">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+    </svg>
+    <h3 class="flex mt-[30px] font-semibold text-black justify-center items-center">Imagem da Empresa</h3>
+    <p class="flex mt-[30px] font-semibold text-black justify-center items-center">Nome da Empresa</p>
+    <p class="flex mt-[70px] font-semibold text-black pl-[50px]">R$99.99</p>
+    <button class="flex mt-[30px] max-w-md mx-auto bg-green-600 hover:bg-green-700 text-black font-semibold justify-center items-center w-[200px] h-[50px]">Comprar</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
