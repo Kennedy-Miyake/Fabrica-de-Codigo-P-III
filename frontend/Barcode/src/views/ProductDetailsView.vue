8b859f499230e2e547b0ac78f2cd8a13173d91ed
Racx-DEV <raulfabiano.dev@gmail.com>
2025-05-29 11:15:28 -0300

    css: style product description field done

--- frontend/Barcode/src/views/ProductDetailsView.vue
+++ frontend/Barcode/src/views/ProductDetailsView.vue
@@ -7,10 +7,10 @@
       <h3 class="text-3xl font-bold text-neutral-800 tracking-tight mb-2">{{ product.name }}</h3>
       <!-- Imagem do Produto -->
       <p class="text-center text-neutral-500 text-lg py-4 break-all">{{ product.imageUrl }}</p>
-
-      <div class="flex flex-col items-center">
-        <h3 class="text-2xl font-bold text-black bg-red-500">Descrição</h3>
-        <p class="text-sm text-black bg-white">{{ product.description }}</p>
+      <!--descriçao-->
+      <div class="w-full flex flex-col items-center">
+        <h4 class="text-lg font-semibold text-neutral-700 mb-1">Descrição</h4>
+        <p class="text-base text-neutral-900 bg-neutral-100 rounded-lg px-4 py-2 text-center w-full border border-neutral-200">{{ product.description }}</p>
       </div>
     </div>
   </section>
