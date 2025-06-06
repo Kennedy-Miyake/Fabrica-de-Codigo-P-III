30164289ca7563261576cc02fe447cddbde40601
Racx-DEV <raulfabiano.dev@gmail.com>
2025-06-06 10:02:54 -0300

    feat: create header cart page

---
+++ frontend/Barcode/src/views/CartView.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="max-w-2xl mx-auto mt-20 bg-white/90 rounded-2xl shadow-xl p-8 border border-neutral-200 relative">
+    <header class="flex justify-between items-center mb-8">
+      <h2 class="text-3xl font-bold text-neutral-800">Seu Carrinho de Compras</h2>
+      <button class="bg-green-600 hover:bg-green-700 text-white px-5 py-2 rounded-xl font-semibold shadow">
+        Finalizar Compra
+      </button>
+    </header>
+   </section> 
+</template>   
