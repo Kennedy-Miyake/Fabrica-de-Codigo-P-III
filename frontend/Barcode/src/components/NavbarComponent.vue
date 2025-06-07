65be0500d3bfbec222aedb8b1a5a63eaee77679e
Racx-DEV <raulfabiano.dev@gmail.com>
2025-06-07 13:24:38 -0300

    feat: create register button in navbar

--- frontend/Barcode/src/components/NavbarComponent.vue
+++ frontend/Barcode/src/components/NavbarComponent.vue
@@ -27,14 +27,22 @@
       <!-- Direita: Login -->
       <div class="flex items-center min-w-max">
         <router-link to="/login" class="ml-4 hover:text-emerald-400 transition">
-          <button class="bg-[#4facfe] text-white font-semibold py-2 px-4 rounded-lg hover:bg-[#8ce7fe] transition-colors shadow-md">
+          <button class="bg-[#4facfe] text-white font-semibold py-2 px-4 rounded-3xl hover:bg-[#8ce7fe] transition-colors shadow-md">
             Login
+          </button>
+        </router-link>
+      </div>
+      <div class="flex items-center min-w-max">
+        <router-link to="/register" class="ml-4 transition">
+          <button class="bg-[#4facfe] text-white font-semibold py-2 px-4 rounded-3xl  hover:bg-[#8ce7fe] transition-colors shadow-md">
+            Regiter
           </button>
         </router-link>
       </div>
     </nav>
     <SideBarComponent v-if="isSidebarOpen" @close="toggleSidebar" />
   </div>
+  
 </template>
 
 <script setup>
